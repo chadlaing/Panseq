7057ac1efe6577d5db056012988d8cbfa1293aed
Chad Laing <chadlaing@gmail.com>
2012-04-20 11:45:12 -0400

    MummerGPU updated; nucmer uses single process now (saves memory for large jobs).

--- lib/Mummer/MummerGPU.pm
+++ lib/Mummer/MummerGPU.pm
@@ -119,63 +119,67 @@ sub run{
 	$self->_g($settingsHashRef->{'g'}) // $self->_g(100);
 	$self->_p($settingsHashRef->{'p'}) // $self->_p('out');
 	
-	my $forkManager = Parallel::ForkManager->new($self->_numberOfCores);
-	{
-		local $/='>';
-		my $refFH = IO::File->new('<'. $self->_referenceFile) or die "$!";
-		
-		my $tempNum=0;		
-		while(<$refFH>){
-			my $fasta=$_;
-			next if $fasta eq '>';
-			
-			my $header;
-			my $seq;
-			if($fasta =~ /(^\N+)\R+/){
-				$header=$1;
-				$seq=$fasta;
-				$seq =~ s/\Q$header//;
-				$seq =~ s/\R//g;
-			}
-			else{
-				$self->logger->fatal("FATAL:\theader: $header");
-				$self->logger->fatal("FATAL:\tseq: $seq");
-				$self->logger->fatal("FATAL:\tfasta: $fasta");
-				exit(1);
-			}
-			
-			$forkManager->start and next();
-				#print out the currentfile as temp
-				my $tempRefFH = File::Temp->new();
-				my $tempQueryFH = File::Temp->new();
-				
-				#create ref temp
-				print $tempRefFH '>'. $header . "\n" . $seq . "\n";
-		
+#	my $forkManager = Parallel::ForkManager->new($self->_numberOfCores);
+#	{
+#		local $/='>';
+#		my $refFH = IO::File->new('<'. $self->_referenceFile) or die "$!";
+#		
+#		my $tempNum=0;		
+#		while(<$refFH>){
+#			my $fasta=$_;
+#			next if $fasta eq '>';
+#			
+#			my $header;
+#			my $seq;
+#			if($fasta =~ /(^\N+)\R+/){
+#				$header=$1;
+#				$seq=$fasta;
+#				$seq =~ s/\Q$header//;
+#				$seq =~ s/\R//g;
+#			}
+#			else{
+#				$self->logger->fatal("FATAL:\theader: $header");
+#				$self->logger->fatal("FATAL:\tseq: $seq");
+#				$self->logger->fatal("FATAL:\tfasta: $fasta");
+#				exit(1);
+#			}
+#			
+#			$forkManager->start and next();
+#				#print out the currentfile as temp
+#				my $tempRefFH = File::Temp->new();
+#				my $tempQueryFH = File::Temp->new();
+#				
+#				#create ref temp
+#				print $tempRefFH '>'. $header . "\n" . $seq . "\n";
+#		
 				#run the mummerGPU
 				my $systemLine = $self->_mummerDirectory;
 				
 				if($self->_gpu == 1){
 					$systemLine .='mummergpu';
-				}
+			}
 				else{
 					$systemLine .='nucmer';
 				}		
 				
-				$systemLine .= ' -b '. $self->_b . ' -c ' . $self->_c . ' -d ' . $self->_d . ' -g ' . $self->_g . ' -l ' . $self->_l . ' -p ' . $self->_baseDirectory . $tempNum . '_temp'. ' ' . $tempRefFH->filename . ' ' . $self->_queryFile;
-				$self->logger->info("INFO:\tsystemLine: $systemLine");			
-				system($systemLine);
-			$forkManager->finish;			
-			
-		}
-		continue{ 
-			$tempNum++;
-		}
-		$refFH->close();		
-	}
-	
-	$forkManager->wait_all_children();
-	$self->_combineDeltaFiles();
+				$systemLine .= ' -b '. $self->_b . ' -c ' . $self->_c . ' -d ' . $self->_d . ' -g ' . $self->_g . ' -l ' . $self->_l . ' -p ' . $self->_baseDirectory . $self->_p . ' ' . $self->_referenceFile . ' ' . $self->_queryFile;
+#				$systemLine .= ' -b '. $self->_b . ' -c ' . $self->_c . ' -d ' . $self->_d . ' -g ' . $self->_g . ' -l ' . $self->_l . ' -p ' . $self->_baseDirectory . $tempNum . '_temp'. ' ' . $tempRefFH->filename . ' ' . $self->_queryFile;
+#			
+#			$self->logger->info("INFO:\tsystemLine: $systemLine");			
+			system($systemLine);
+			$self->deltaFile($self->_baseDirectory . $self->_p . '.delta');
+			$self->logger->info("INFO:\tDelta file: " . $self->deltaFile);
+#			$forkManager->finish;			
+#			
+#		}
+#		continue{ 
+#			$tempNum++;
+#		}
+#		$refFH->close();		
+#	}
+#	
+#	$forkManager->wait_all_children();
+#	$self->_combineDeltaFiles();
 }
 
 
