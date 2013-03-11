32af1eea16a73f4d423b7a831ebb2e1323898bd1
Chad Laing <chadlaing@gmail.com>
2013-03-11 10:51:57 -0600

    modified flexible printer to use only FH, not filenames

--- lib/Roles/FlexiblePrinter.pm
+++ lib/Roles/FlexiblePrinter.pm
@@ -8,53 +8,39 @@ use FindBin;
 use IO::File;
 use lib "$FindBin::Bin/../";
 use Role::Tiny;
- 
+
 sub outputFH{
-	my $self =shift;
-	
-	if(@_){
-		$self->{'_outputFilehandle'}=shift;
-	}
-	else{
-		if(defined $self->{'_outputFilehandle'}){
-			return $self->{'_outputFilehandle'};
-		}
-		elsif(defined $self->outputFile){
-			$self->{'_outputFilehandle'}= (IO::File->new('>' . $self->outputFile) or die "Could not create FH in Roles::FlexiblePrinter");
-			return $self->{'_outputFilehandle'};
-		}
-		else{
-			return *STDOUT{IO};
-		}
-	}
+	my $self=shift;
+	$self->{'_outputFH'}=shift // return $self->{'_outputFH'};
 }
+
 
 #methods
 sub printOut{
 	my($self)=shift;
 	
-	my $outFH = $self->outputFH;
+	my $outFH = $self->outputFH // *STDOUT{IO};
 	print $outFH @_;
 }
 
-sub outputFile{
-	my $self=shift;
-	$self->{'_outputFile'}=shift // return $self->{'_outputFile'};
-}
+# sub outputFile{
+# 	my $self=shift;
+# 	$self->{'_outputFile'}=shift // return $self->{'_outputFile'};
+# }
 
-sub setNewOutputFile{
-	my $self=shift;
-	my $newFile=shift;
+# sub setNewOutputFile{
+# 	my $self=shift;
+# 	my $newFile=shift;
 
-	if(defined $newFile){
-		if($self->outputFH){
-			$self->outputFH->close();
-		}
+# 	if(defined $newFile){
+# 		if($self->outputFH){
+# 			$self->outputFH->close();
+# 		}
 		
-		$self->outputFile($newFile);
-		$self->outputFH(IO::File->new('>' . $newFile) or die "Could not create FH from $newFile in Roles::FlexiblePrinter");
-	}
-}
+# 		$self->outputFile($newFile);
+# 		$self->outputFH(IO::File->new('>' . $newFile) or die "Could not create FH from $newFile in Roles::FlexiblePrinter");
+# 	}
+# }
 
 sub DESTROY{
 	my $self=shift;
