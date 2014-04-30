3025cc9ece65976551a79c73cf94ff6ca5510c26
Chad Laing <chadlaing@gmail.com>
2014-04-30 11:21:52 -0600

    All tests pass, ready for Master merge.

--- lib/panseq.pl
+++ lib/panseq.pl
@@ -10,13 +10,13 @@ use Log::Log4perl qw/:easy/;
 
 #need to get the settings up-front to allow log-file output
 my $settings = Modules::Setup::Settings->new($ARGV[0]);
-print "settings file: $ARGV[0]\n";
+print "Settings file: $ARGV[0]\n";
 
 #MUMmmer defaults its messages to STDERR
 #we want them logged
 #closing STDERR and associating it with Log4perl is done below
-#close STDERR;
-#tie *STDERR, "Tie::Log4perl";
+close STDERR;
+tie *STDERR, "Tie::Log4perl";
 
 my $panseq = Modules::Setup::Panseq->new($settings);
 Log::Log4perl->init("$FindBin::Bin/log4p.conf");
