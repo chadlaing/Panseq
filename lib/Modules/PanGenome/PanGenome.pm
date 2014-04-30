5f2b007f5f306d2fb2e4011b6b70ea8eff3f7a7f
Chad Laing <chadlaing@gmail.com>
2014-04-30 10:52:43 -0600

    Moved the addition to binary and snp tables within the first hit only for a result. Only the alleles file gets all copies.

--- lib/Modules/PanGenome/PanGenome.pm
+++ lib/Modules/PanGenome/PanGenome.pm
@@ -735,60 +735,62 @@ sub _processBlastXML {
 						sequence=>$hit->[10]
 					);
 				}						
-											
-				$self->_insertIntoDb(
-					table=>'results',
-					type=>'binary',
-					contig_id=>$contigId,
-					locus_id=>$counter,
-					number=>$counter,
-					start_bp=>$hit->[2],
-					end_bp=>$hit->[3],
-					value=>1
-				);				
-			}	
-		
-			if($coreOrAccessory eq 'core'){
 				
-				#generate a MSA of all strains that contain sequence
-				#if the locus is core, we will send this MSA to the SNPFinder
-				my $msaHash = $self->_getHashOfFastaAlignment(
-					$self->_getMsa($result,$counter),
-					$result
-				);
-				
-				#'outfmt'=>'"6 
-				# [0]sseqid 
-				# [1]qseqid 
-				# [2]sstart 
-				# [3]send 
-				# [4]qstart 
-				# [5]qend 
-				# [6]slen 
-				# [7]qlen 
-				# [8]pident 
-				# [9]length"',
-				# [10]sseq,
-				# [11]qseq		
-	
-				#if it is a core result, send to SNP finding
-				$self->logger->debug("Core, adding to DB");
-				my $coreResults = $self->_getCoreResult($result,$msaHash,$counter);		
-				foreach my $cResult(@{$coreResults}){								
+				if($hitNum == 1){
 					$self->_insertIntoDb(
 						table=>'results',
-						type=>'snp',
-						contig_id=>$self->_contigIds->{$cResult->{'contig'}},
-						number=>$cResult->{'locusId'},
+						type=>'binary',
+						contig_id=>$contigId,
 						locus_id=>$counter,
-						start_bp=>$cResult->{'startBp'},
-						end_bp=>$cResult->{'startBp'},
-						value=>$cResult->{'value'}
-					);
-				}
-			}
-		}
-	}
+						number=>$counter,
+						start_bp=>$hit->[2],
+						end_bp=>$hit->[3],
+						value=>1
+					);	
+					
+					if($coreOrAccessory eq 'core'){
+						
+						#generate a MSA of all strains that contain sequence
+						#if the locus is core, we will send this MSA to the SNPFinder
+						my $msaHash = $self->_getHashOfFastaAlignment(
+							$self->_getMsa($result,$counter),
+							$result
+						);
+						
+						#'outfmt'=>'"6 
+						# [0]sseqid 
+						# [1]qseqid 
+						# [2]sstart 
+						# [3]send 
+						# [4]qstart 
+						# [5]qend 
+						# [6]slen 
+						# [7]qlen 
+						# [8]pident 
+						# [9]length"',
+						# [10]sseq,
+						# [11]qseq		
+			
+						#if it is a core result, send to SNP finding
+						$self->logger->debug("Core, adding to DB");
+						my $coreResults = $self->_getCoreResult($result,$msaHash,$counter);		
+						foreach my $cResult(@{$coreResults}){								
+							$self->_insertIntoDb(
+								table=>'results',
+								type=>'snp',
+								contig_id=>$self->_contigIds->{$cResult->{'contig'}},
+								number=>$cResult->{'locusId'},
+								locus_id=>$counter,
+								start_bp=>$cResult->{'startBp'},
+								end_bp=>$cResult->{'startBp'},
+								value=>$cResult->{'value'}
+							);
+						} #foreach cResult
+					}#if core			
+				} #if hitnum ==1							
+			}#foreach hit	
+		}#foreach resKey
+	}#while result
 	$self->logger->info("Total results: $totalResults");
 	$self->_emptySqlBuffers();
 }
