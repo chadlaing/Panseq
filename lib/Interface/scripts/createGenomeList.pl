971f4b73a01b626f9d91d0034573a385a5b87c16
Chad Laing <chadlaing@gmail.com>
2017-02-22 11:42:01 -0700

    Updated server to take into account the new GenBank directory structure. Fixes problem where GenBank genomes were no longer being downloaded.

--- lib/Interface/scripts/createGenomeList.pl
+++ lib/Interface/scripts/createGenomeList.pl
@@ -4,37 +4,37 @@ use warnings FATAL => 'all';
 use File::Copy;
 
 my $genomeList = $ARGV[0] // die "no genome list file specified!\n";
-my $fullFile = $ARGV[1] // die "no full HTML file specified!\n";
-my $reducedFile = $ARGV[2] // die "no reduced HTML file specified!";
+my $type = $ARGV[1] // die "no type of 'pan' or 'novel' specified!\n";
 
 
 
 #we will make a _full and _reduced version to facilitate page loading
 #_full will contain the entire set of genbank data (~15MB HTML file)
 #_reduced version will contain every Nth item
-#make a copy of the original, read it in
-#write out over the original, with the new data
-#keep the temp file around for disasters
 
-#with File::Copy
-my $fullTempFile = $fullFile . '_temp';
-my $reducedTempFile = $reducedFile . '_temp';
-copy($fullFile, $fullTempFile) or die "Could not create temp full html file!\n";
-copy($reducedFile, $reducedTempFile) or die "Could not create temp reduced html file\n";
+#use the _base.html versions of novel and pan, to create the reduced and full
+#version of each page
+#the base version has no list elements, but is otherwise identical
+#and includes the following text
+
+my $REDUCED_TEXT = q{
+    <p>
+        IMPORTANT: To speed page loading, this view contains only 1% of the total genomes available for comparison. To load the complete set of genomes, please click <a href="/panseq/page/novel_full.html">Full Genome Set</a>
+    </p>
+};
+
 
 #write out the new html file with new data
 open(my $genomeFH, '<', $genomeList) or die "$!";
-open(my $fullFH, '>', $fullFile) or die "$!";
-open(my $reducedFH, '>', $reducedFile) or die "$!";
-
 
 my %fullHash;
 while(my $line = $genomeFH->getline()){
-    next if $. == 1;
+    next if $. == 1 || $. == 2;
 
     $line =~ s/\R//g;
     my @la = split('\t', $line);
 
+    #19 is organism path
     my $organismName = $la[7];
     my $strainName = $la[8];
     my $isolateName = $la[9];
@@ -59,64 +59,73 @@ while(my $line = $genomeFH->getline()){
     }
 
     my $ftpName;
-    if($la[19] =~ m/genomes\/all\/(.+)/){
+    if($la[19] =~ m/all\/(.+)$/){
         $ftpName= $1;
     }
     else{
-        die "Could not get FTP location for $organismName\n";
+        die "Could not get FTP location for $organismName from $la[19]\n";
     }
     $fullHash{$ftpName}=$fullName;
 }
 
-open(my $tempFH, '<', $reducedTempFile) or die "Could not open $reducedTempFile $!\n";
-my $switch = 0;
-my $switchCounter = 0;
-my $reducedOnly = 0;
-my $prefix = 'q_';
-while(my $line = $tempFH->getline()){
-    if($switch == 1){
-        #skip all the existing list elements
-        if($line =~ m/\<li\>/){
-            next;
+#go through base file
+my $reducedFile;
+my $fullFile;
+my $baseFile;
+
+if($type eq 'pan'){
+    $reducedFile = 'pan.html';
+    $fullFile = 'pan_full.html';
+    $baseFile = 'pan_base.html';
+}
+elsif($type eq 'novel'){
+    $reducedFile = 'novel.html';
+    $fullFile = 'novel_full.html';
+    $baseFile = 'novel_base.html';
+}
+else{
+    die "Invalid type! Please specify novel or pan\n";
+}
+
+open(my $fullFH, '>', $fullFile) or die "$!";
+open(my $reducedFH, '>', $reducedFile) or die "$!";
+
+my $prefix;
+
+open(my $baseFH, '<', $baseFile) or die "Couldn't open $baseFile $!\n";
+
+while(my $line = $baseFH->getline()){
+    if($line =~ m/--Reduced Only--/){
+        $reducedFH->print($REDUCED_TEXT);
+    }
+
+    # <li><input type="checkbox" name="q_GCA_000160075.2_ASM16007v2">Abiotrophia defectiva ATCC 49176 </li>
+    if($line =~m/--Query--/ || $line =~ m/--Reference--/){
+        if($line =~ m/--Query--/){
+            $prefix = 'q_';
         }
         else{
-            my $counter =0;
-            foreach my $k(sort {$fullHash{$a} cmp $fullHash{$b}} keys %fullHash){
-                if($counter % 100 == 0){
-                    $reducedFH->print('<li><input type="checkbox" name="' . $prefix . $k  . '">' . $fullHash{$k} . '</li>' . "\n");
-                }
-
-                $fullFH->print('<li><input type="checkbox" name="' . $prefix . $k  . '">' . $fullHash{$k} . '</li>' . "\n");
-                $counter++;
-            }
-            $switch=0;
-        }
-    }
-
-    if($line =~ m/\<\!--Reduced Only--\>/){
-        $reducedOnly++;
-    }
-
-    if($reducedOnly > 0){
-        $reducedFH->print($line);
-    }
-    else{
-        $fullFH->print($line);
-        $reducedFH->print($line);
-    }
-
-    if($reducedOnly == 2){
-        $reducedOnly=0;
-    }
-
-
-    if($line =~ m/\Flip the switch/){
-        $switch = 1;
-        $switchCounter++;
-
-        if($switchCounter == 2){
             $prefix = 'r_';
         }
+
+        my $count=0;
+        foreach my $k(sort {$fullHash{$a} cmp $fullHash{$b}} keys %fullHash){
+            my $listLine = '<li><input type="checkbox" name="'. $prefix . $k . '">' . $fullHash{$k} . '</li>';
+
+            #only print every 100 items to the reduced file
+            if(($count % 100) == 0){
+                $reducedFH->print($listLine . "\n");
+            }
+            $fullFH->print($listLine . "\n");
+
+            $count++;
+        }
+    }
+    else{
+        $reducedFH->print($line);
+        $fullFH->print($line);
     }
 }
+print "Finished\n";
+
 
